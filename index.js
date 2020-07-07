29a2a63f71567b76c469c7e8c24e4115e89181f0
nikita <freenikita95@gmail.com>
2020-07-08 00:59:44 +0300

    basic setting for graphQL

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const path = require('path');
+const graphqlHTTP = require('express-graphql')
 
-const todoRout = require('./router/todo')
+const schema = require('./graphql/schema')
+const resolver = require('./graphql/resolver')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -10,12 +12,31 @@ const PORT = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
-app.use('/api/todo', todoRout)
+// app.use(graphqlHTTP({
+//     schema,
+//     rootValue: resolver,
+// }))
+
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 app.use((req, res, next) => {
     res.sendFile('/index.html')
 })
-
 
 const MONGO_CONNECT = "mongodb+srv://nikita:JaB5sqmHVoyLzfaT@cluster0-boop9.mongodb.net/restAPI?retryWrites=true&w=majority"
 
@@ -28,7 +49,7 @@ async function start(){
         })
 
         app.listen(PORT, () => {
-            console.log(`The server was run on port ${PORT}`)
+            console.log(`The server was run on port ${PORT}${server.graphqlPath}`)
         })
 
     } catch(error) {
